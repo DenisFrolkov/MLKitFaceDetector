dc38d49a5f5488597fbf2cb6ff541e8db4bd27b1
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-02-03 15:09:40 +0300

    Camera mapping implemented in the application.

--- app/src/main/java/com/denis/mlkitfacedetector/MainActivity.kt
+++ app/src/main/java/com/denis/mlkitfacedetector/MainActivity.kt
@@ -1,29 +1,81 @@
 package com.denis.mlkitfacedetector
 
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.Manifest
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.camera.core.CameraSelector
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import android.content.Context
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.Preview
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.LifecycleOwner
 import com.denis.mlkitfacedetector.ui.theme.MLKitFaceDetectorTheme
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        if (permissionGranted()) {
+            initView()
+        } else {
+            requestPermission()
+        }
+
+    }
+
+    private fun permissionGranted() = ContextCompat.checkSelfPermission(
+        this,
+        Manifest.permission.CAMERA
+    ) == PackageManager.PERMISSION_GRANTED
+
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.CAMERA),
+            0
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray,
+        deviceId: Int
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId)
+        if (requestCode == 0) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                initView()
+            } else {
+                Toast.makeText(this, "Camera Permission Denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun initView() {
         setContent {
             MLKitFaceDetectorTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CameraPreview(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +83,57 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun CameraPreview(
+    modifier: Modifier = Modifier,
+    cameraSelector: CameraSelector = CameraSelector.DEFAULT_BACK_CAMERA,
+    scaleType: PreviewView.ScaleType = PreviewView.ScaleType.FILL_CENTER,
+) {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val previewView = remember { PreviewView(context) }
+
+    val cameraProviderFuture = remember {
+        ProcessCameraProvider.getInstance(context).configureCamera(previewView, lifecycleOwner, cameraSelector, context)
+    }
+
+    AndroidView(
+        modifier = modifier,
+        factory = {
+            previewView.apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+                implementationMode = PreviewView.ImplementationMode.COMPATIBLE
+            }
+        }
     )
 }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MLKitFaceDetectorTheme {
-        Greeting("Android")
-    }
+private fun ListenableFuture<ProcessCameraProvider>.configureCamera(
+    previewView: PreviewView,
+    lifecycleOwner: LifecycleOwner,
+    cameraSelector: CameraSelector,
+    context: Context
+): ListenableFuture<ProcessCameraProvider> {
+    addListener({
+        val preview = Preview.Builder()
+            .build()
+            .apply {
+                setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+        try {
+            get().apply {
+                unbindAll()
+                bindToLifecycle(
+                    lifecycleOwner, cameraSelector, preview
+                )
+            }
+        } catch (exc: Exception) {
+            TODO("process errors")
+        }
+    }, ContextCompat.getMainExecutor(context))
+    return this
 }
