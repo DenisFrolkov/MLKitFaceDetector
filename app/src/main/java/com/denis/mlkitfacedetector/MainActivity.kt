70747f4c13598072a238a0894de8df6d88404449
Denis Frolkov <99652468+DenisFrolkov@users.noreply.github.com>
2025-02-03 14:14:45 +0300

    Initial commit

---
+++ app/src/main/java/com/denis/mlkitfacedetector/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.denis.mlkitfacedetector
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.denis.mlkitfacedetector.ui.theme.MLKitFaceDetectorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MLKitFaceDetectorTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    MLKitFaceDetectorTheme {
+        Greeting("Android")
+    }
+}
